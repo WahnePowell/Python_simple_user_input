95076b18a8cce0629b473017f8e1e133ceda6ccb
Wahne Powell <80147268+WahnePowell@users.noreply.github.com>
2021-11-20 18:09:31 -0800

    Add files via upload

---
+++ simpleUserInput.py
@@ -0,0 +1,12 @@
+# Simple user input with numbers
+# https://github.com/wahnepowell
+
+num1 = input("Enter a number: ")
+print("The number you entered is: " + num1)
+
+num2 = input("Enter a second number: ")
+print("The second number you entered is: " + num2)
+
+result = int(num1) + int(num2)
+
+print(num1 + " + " + num2 + " =", result)
